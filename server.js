11ac3ffeb7d20cf21d9a8886302f7f04c7f6f53a
Ike <ikeuzo57@gmail.com>
2016-07-26 22:39:36 +0000

    add/todos and todos/:id

--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 var express = require('express');
 var app = express();
 
+var todos = [{
+    id: 1,
+    description: 'Meet mom for launch',
+    completed: false,
+},{
+    id:2,
+    description:'Go to Market',
+    completed: false
+}, {
+    id: 3,
+    description: 'Go to the Mall',
+    completed:true
+},{
+    id: 4,
+    description: 'Go to Library',
+    completed:true
+}]
+
+
 // Route
 app.get('/', function(req,res){
     res.send('Todo API Root');
 })
 
-
+// get/todos
+app.get('/todos', function(req,res){
+    res.json(todos);
+})
+// get/todos/:id
+app.get('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+    todos.forEach(function(todo){
+        if(todoId === todo.id){
+            matchedTodo = todo;
+        }
+    });
+    if(matchedTodo){
+        res.json(matchedTodo)
+    }else{
+        res.status(404).send()
+    }
+})
 
 
 
